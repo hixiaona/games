3bfd58b4a507c98ec85cf449f18237ae4a6a4168
hixiaona <48093434+hixiaona@users.noreply.github.com>
2019-03-05 23:33:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/games/pass.java
@@ -0,0 +1,14 @@
+package com.example.games;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class pass extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pass);
+    }
+}
